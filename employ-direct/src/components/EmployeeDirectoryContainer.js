e1d00533988e8c75d767042f0090e7b0fda9899d
CaroKris <ckhilpisch@gmail.com>
2021-04-23 17:38:33 -0500

    importing necessary components in main page, attempting a function to create the users with the api call

---
+++ employ-direct/src/components/EmployeeDirectoryContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import EmployeeTabke from "./EmployeeTable";
+import Header from "./Header";
+import getUserData from "../utils/API";
+
+
+
+
+// createUsers(() => {
+//     async function apiCall() {
+//         try {
+//             const data = await getUserData();
+//             console.log(data.results)
+//         }
+//     };
+// })
