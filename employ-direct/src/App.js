7440335b92359d39bbcb21aa90a5ba1b8ac2217b
CaroKris <ckhilpisch@gmail.com>
2021-04-23 15:29:44 -0500

    creating boiler plate files for the React app

---
+++ employ-direct/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import EmployeeDirectoryContainer from "./components/EmployeeDirectoryContainer.js";
+
+function App() {
+  return <EmployeeDirectoryContainer />;
+}
+
+export default App;
